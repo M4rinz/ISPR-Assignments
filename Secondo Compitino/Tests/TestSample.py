8d1b606ee51ae7df06840f260384a062e2506058
Andrea Marino <134485532+M4rinz@users.noreply.github.com>
2024-04-06 10:24:01 +0200

    Enriched test with wrong assignment

--- Secondo Compitino/Tests/TestSample.py
+++ Secondo Compitino/Tests/TestSample.py
@@ -24,7 +24,7 @@ init_dict = {
     frozenset([('primo',0)]) : 0.1,
     frozenset([('primo',1)]) : 0.99
 }
-CPTprova = CPT(init_dict, n2.BS)
+CPTprova = CPT(init_dict, parents_list=n2.BS, node_label=n2.label)
 n1.distribution = PriorBernoulli(0.001)
 
 CPTprova.sample()
@@ -36,3 +36,16 @@ for _ in range(10000):
 s = s / 10000
 # very rough
 assert s > 0.09 and s < 0.11
+
+
+
+# wrong assignment:
+n3 = Node(label='terzo')
+BN1.add_nodes(n3)
+BN1.add_arcs((3,2))
+
+wrong_init_dict = {
+    frozenset([('primo',0),('terzo',0)]) : 0.1,
+    frozenset([('primo',1)]) : 0.99
+}
+CPTwrong = CPT(wrong_init_dict, n2.BS, n2.label)
