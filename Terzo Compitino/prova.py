ad83f003ad256675384af1d42c89b16e6cfe99a2
Andrea Marino <134485532+M4rinz@users.noreply.github.com>
2024-05-18 15:20:07 +0200

    HorseshoeNetwork corrected

--- Terzo Compitino/prova.py
+++ Terzo Compitino/prova.py
@@ -3,86 +3,123 @@ import torch.nn as nn
 from typing import List, Tuple
 
 class HorseshoeEncoder(nn.Module):
-    def __init__(self, architecture: List[Tuple[int, int]] = [(1, 16), (1, 32)]) -> None:
+    def __init__(self,
+                 in_channels:int=3,
+                 architecture:List[Tuple]=[(2,16),(2,32),(3,64),(3,128),(3,128)],) -> None:
         super(HorseshoeEncoder, self).__init__()
 
-        def conv_layer(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        def conv_layer(in_channels, out_channels, *args, **kwargs):
             return nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+                nn.Conv2d(in_channels, out_channels, *args, **kwargs),
                 nn.BatchNorm2d(out_channels),
                 nn.ReLU()
             )
         
         self.layers = nn.ModuleDict()
-        in_channels = 3  # Initial number of input channels for the first convolution layer
 
+        in_channels = in_channels  # initial number of input channels
         for i, (n_blocks, out_channels) in enumerate(architecture):
             conv_layers = []
             for _ in range(n_blocks):
-                conv_layers.append(conv_layer(in_channels, out_channels))
+                conv_layers.append(conv_layer(in_channels, out_channels, kernel_size=3, stride=1, padding=0))
                 in_channels = out_channels
             conv_block = nn.Sequential(*conv_layers)
-            self.layers.add_module(f'conv_block_{i}', nn.ModuleList([conv_block, nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)]))
+            # We add a MaxPooling layer after the prescribed number of conv layers
+            self.layers.add_module(f'conv_block_{i}',nn.ModuleList([conv_block, nn.MaxPool2d(2,2,return_indices=True)]))
 
         self.conv1x1 = nn.Conv2d(in_channels, 256, kernel_size=1, stride=1, padding=0)
 
     def forward(self, x):
         pooling_indices = []
         input_sizes = []
-
-        for block in self.layers.values():
-            conv_block, pool_layer = block
+        #for block in self.layers.values():
+        for i in range(len(self.layers)):
+            conv_block, pooling_layer = self.layers[f'conv_block_{i}']
+            # Apply the convolutional layer
             x = conv_block(x)
+            # we need to keep track of the input size
+            # before the pooling, to do the unpooling correctly at decoding time
             input_sizes.append(x.size())
-            x, indices = pool_layer(x)
+            # Apply the pooling layer
+            x, indices = pooling_layer(x)
+            # We need to keep track of the indices for the unpooling
             pooling_indices.append(indices)
-        
         x = self.conv1x1(x)
 
         return x, pooling_indices, input_sizes
 
 class HorseshoeDecoder(nn.Module):
-    def __init__(self, architecture: List[Tuple[int, int]] = [(1, 32), (1, 16)]) -> None:
+    def __init__(self,
+                 out_channels:int=1,
+                 architecture:List[Tuple]=[(2,16),(2,32),(3,64),(3,128),(3,128)],) -> None:
         super(HorseshoeDecoder, self).__init__()
 
-        def transp_conv_layer(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        def transp_conv_layer(in_channels, out_channels, *args, **kwargs):
             return nn.Sequential(
-                nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+                nn.ConvTranspose2d(in_channels, out_channels, *args, **kwargs),
                 nn.BatchNorm2d(out_channels),
                 nn.ReLU()
             )
-
+        
         self.layers = nn.ModuleDict()
 
-        _, in_channels = architecture[0]
+        out_channels = out_channels     # desired n° of output channels
+        arch_len = len(architecture)
+        for i, (n_blocks, in_channels) in enumerate(architecture):
+            transp_conv_layers = []
+            for _ in range(n_blocks):
+                transp_conv_layers.insert(0,transp_conv_layer(in_channels, out_channels, kernel_size=3, stride=1, padding=0))
+                out_channels = in_channels
+            transp_conv_block = nn.Sequential(*transp_conv_layers)
+            # Since we're building the architecture from right to left, we need to add 
+            # (more precisely, index) the modules in reversed order in the dictionary
+            self.layers.add_module(f'transp_conv_block_{arch_len-i-1}',nn.ModuleList([nn.MaxUnpool2d(2,2), transp_conv_block])) 
 
         self.Tconv1x1 = nn.ConvTranspose2d(256, in_channels, kernel_size=1, stride=1, padding=0)
 
-        for i, (n_blocks, out_channels) in enumerate(architecture):
-            transp_conv_layers = []
-            for _ in range(n_blocks):
-                transp_conv_layers.append(transp_conv_layer(in_channels, out_channels))
-                in_channels = out_channels
-            transp_conv_block = nn.Sequential(*transp_conv_layers)
-            self.layers.add_module(f'transp_conv_block_{i}', nn.ModuleList([nn.MaxUnpool2d(kernel_size=2, stride=2), transp_conv_block]))
-
-        self.final_conv1x1 = nn.ConvTranspose2d(in_channels, 1, kernel_size=1, stride=1, padding=0)
-
     def forward(self, x, pooling_indices, input_sizes):
         x = self.Tconv1x1(x)
-        for block in self.layers.values():
-            unpool_layer, transp_conv_block = block
-            x = unpool_layer(x, pooling_indices.pop(), output_size=input_sizes.pop())
+        #for block in reversed(self.layers.values()):
+        for i in range(len(self.layers)):
+            unpooling_layer, transp_conv_block = self.layers[f'transp_conv_block_{i}']
+            # First the unpooling (which needs the indices used for pooling and the size of the input)...
+            x = unpooling_layer(x, pooling_indices.pop(), output_size=input_sizes.pop())
+            # ... then the transposed convolution!
             x = transp_conv_block(x)
-        x = self.final_conv1x1(x)
+
         return x
+    
+class HorseshoeNetwork(nn.Module):
+    def __init__(self,
+                 in_channels=3,
+                 out_channels=1,
+                 architecture:List[Tuple]=[(2,16),(2,32),(3,64),(3,128),(3,128)],):
+        super(HorseshoeNetwork, self).__init__()
+
+        # Convolutional layers
+        self.encoder = HorseshoeEncoder(architecture=architecture,in_channels=in_channels)
+
+        # Transpose conv. layers
+        self.decoder = HorseshoeDecoder(architecture=architecture,out_channels=out_channels)
+        
+    def forward(self, x):
+        # Encoding
+        x, pooling_indices, input_sizes = self.encoder(x)
+
+        # Decoding
+        x = self.decoder(x, pooling_indices, input_sizes)
+
+        # Final sigmoid (classification)
+        x = torch.sigmoid(x)
+        return x
+
 
 # Test with an input image tensor of shape [1, 3, 568, 800]
 img = torch.randn(1, 3, 568, 800)
 
 # Initialize the encoder and decoder with the specified architectures
-encoder = HorseshoeEncoder(architecture=[(1, 16), (1, 32)])
-decoder = HorseshoeDecoder(architecture=[(1, 32), (1, 16)])
+encoder = HorseshoeEncoder(architecture=[(2, 16), (2, 32)])
+decoder = HorseshoeDecoder(architecture=[(2, 16), (2, 32)])
 
 # Move to device if necessary (for example, GPU)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
